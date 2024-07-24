e8c3d36ce064a91c91623b013129a756113df2ed
bhumitiwari <bhumikat2910@gmail.com>
2024-07-24 19:26:33 +0530

    added mongodb url

--- backend/connect/conn.js
+++ backend/connect/conn.js
@@ -1,8 +1,8 @@
 const mongoose=require("mongoose");
-
+const DB=process.env.DB;
 const conn=async()=>{
     try{
-await mongoose.connect("mongodb://localhost:27017/adoption");
+await mongoose.connect(DB);
 console.log("Connected to database");
     }
     catch(error)
